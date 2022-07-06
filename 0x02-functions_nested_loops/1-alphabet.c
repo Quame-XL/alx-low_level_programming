1fa9e7ce379c362fdd24eff2806b5b7abb56046a
Quame-XL <quamemessel@gmail.com>
2022-07-06 11:43:15 -0700

    Corrects error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,15 @@
 
 /**
  * print_alphabet - prints alphabets in lowercase
- *
- * Return: Always 0
  */
 
 void print_alphabet(void)
 {
 	int alph;
 
-	for (alph = 97; alph <= 122; alph++)
+	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		_putchar(alph);
 	}
 	_putchar('\n');
-
-	return (0);
 }
