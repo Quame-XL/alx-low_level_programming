90d19ef846360960317826f383bab0b79138b4fa
Quame-XL <quamemessel@gmail.com>
2022-07-06 11:09:18 -0700

    Prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in lower case
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int alph;
+	for (alph = 97; alph <= 122; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+	return (0)
+}
