efe486b8768afcf16b2cb8b8f66342ba4d795127
Quame-XL <quamemessel@gmail.com>
2022-07-09 23:38:44 +0000

    Checks digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: integer argument
+ * Return: 0 or 1 based on conditions
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
