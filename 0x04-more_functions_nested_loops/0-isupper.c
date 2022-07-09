59af000c62fe11d405dc5349cbc485743a778b3e
Quame-XL <quamemessel@gmail.com>
2022-07-09 23:29:22 +0000

    Checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: interger argument
+ * Return: 1 or 0 based on conditions
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
