98cda2b6b99ee5ea84584a70f6d750ac53aa057d
Quame-XL <quamemessel@gmail.com>
2022-07-20 00:47:45 +0000

    Prints sums of diagonals of matrices

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,14 +18,14 @@ void print_diagsums(int *a, int size)
 
 	z1 = (size * size) - 1;
 
-	for (x = 0; x <= z1; x = x +(size + 1);)
+	for (x = 0; x <= z1; x = x +(size + 1))
 	{
-		s1 += s1 + a[x];
+		s1 = s1 + a[x];
 	}
-	for (x = (size - 1); x < z1; x = x + (size - 1)
+	for (x = (size - 1); x < z1; x = x + (size - 1))
 	{
 		s2 = s2 + a[x];
 	}
 
-	printf("%d, %d\n, s1, s2);
+	printf("%d, %d\n", s1, s2);
 }
